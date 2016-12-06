ef8157167308b15e4b5cf983aa560f9cfce0be87
Jorge Avante Reyes <jorge.avante-reyes@kaufland.com>
2016-12-06 13:12:43 +0100

    initial commit

---
+++ src/main/java/com/avante/BirthdaysApiApplication.java
@@ -0,0 +1,12 @@
+package com.avante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirthdaysApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirthdaysApiApplication.class, args);
+	}
+}
